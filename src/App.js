519641962552f074a646445baba2e5f5acf7f949
Saul <saul7duran7@hotmail.com>
2019-08-24 21:36:23 -0600

    ReactTVDemoUpdate

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Logo from './logo.svg';
+import './App.css';
+import ManagePage from './pages/managepage/manage-page'
+import PreviewPage from './pages/previewpage/preview-page'
+import Navigation from './components/navigation/navigation'
+import {Route} from 'react-router-dom'
+function App() {
+  return(
+    <div className="app">
+     <Navigation/>
+    <Route exact path = "/" component = {ManagePage} />
+    <Route exact path = "/preview-page" component = {PreviewPage} />
+   
+    </div>
+    
+  );
+  } 
+
+
+  export default App;
+
